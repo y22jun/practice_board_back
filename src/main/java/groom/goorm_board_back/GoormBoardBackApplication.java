2d41a58e26626939eae99c885377d512c8b1d790
jjh75607 <dkrak5738@naver.com>
2024-08-29 21:48:45 +0900

    project init

---
+++ src/main/java/groom/goorm_board_back/GoormBoardBackApplication.java
@@ -0,0 +1,13 @@
+package groom.goorm_board_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoormBoardBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoormBoardBackApplication.class, args);
+    }
+
+}
